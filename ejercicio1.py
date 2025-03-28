f46283294d714d7234ef73bc46ec8397dceee1f5
AlfredoMontealegre <montealfredo24@gmail.com>
2025-03-28 08:28:36 -0600

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,5 @@
+# haciendo pruebas 
+print ("Hola mundo, me llamo jose")
+print ("Hola mundo, me llamo jose")
+print ("Hola mundo, jose me llamo ")
+print ("Alfredo", "Montealegre", "Nicaragua",  sep ="🔥", end="🥂")
