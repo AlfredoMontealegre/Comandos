511433b6c49e1526a683c90aa554d8ebe4a4df97
AlfredoMontealegre <montealfredo24@gmail.com>
2025-03-31 17:50:05 -0600

    PythonWorks

---
+++ Circulo.py
@@ -0,0 +1,6 @@
+# Circulo
+radio = int(input("Agrege el radio del circulo:"))
+
+area = (radio * radio) * 3.1416
+
+print("El Area del Circulo es:", area)
---
+++ DolaresACordobas.py
@@ -0,0 +1,6 @@
+# Dolares a Cordobas
+dolares = int(input("Agrege una cantidad en Dolares"))
+
+cordobas = dolares * 36.7
+
+print("La cantidad en Cordobas es: ", cordobas)
